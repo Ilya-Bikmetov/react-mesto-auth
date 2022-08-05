3b82300a1180488448e983745f1ce059b524e933
Ilya Bikmetov <ilya-bikmetov@yandex.ru>
2022-08-06 00:45:36 +0700

    feat/header_switch_profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
-import { Link, useHistory, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header({ loggedIn, userInfo: { email }, onSignout }) {
   const location = useLocation();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleOpenProfile = () => setIsClicked(true);
+  const handleCloseProfile = () => setIsClicked(false);
 
   return (
-    // <header className="header">
-    //   <div className="header__logo"></div>
-    <div>
+    <>
       {
         !loggedIn &&
         <header className="header">
@@ -20,16 +23,24 @@ function Header({ loggedIn, userInfo: { email }, onSignout }) {
 
       {
         loggedIn &&
-        <header className="header header_transform">
-          <div className="header__logo"></div>
+        <header className="header header_mobile">
+          <div className="header__container">
+            <div className="header__logo header__logo_mobile" />
+            <div onClick={handleOpenProfile} className={`header__btn ${isClicked && 'header__btn_off'} header__btn_open`}>
+              <div className="header__btn__element" />
+              <div className="header__btn__element" />
+              <div className="header__btn__element" />
+            </div>
+            <button onClick={handleCloseProfile} className={`header__btn ${!isClicked && 'header__btn_off'} header__btn_close`} />
+          </div>
           {location.pathname === "/" &&
-            <nav className="header__links">
+            <nav className={`header__links ${!isClicked && 'header__links_off'} header__links_mobile`}>
               <p className="header__link header__link_text">{email}</p>
               <Link onClick={onSignout} className="header__link" to={"./sign-in"}>Выйти</Link>
             </nav>}
         </header>
       }
-    </div>
+    </>
 
   );
 }
